2bb94b3b07f166187eca3ee9f6cd8aab158627d6
Spiros Eliopoulos <seliopou@gmail.com>
2013-07-26 20:54:31 +0300

    ast: add surface syntax

--- Language/Typo/ASTs.hs
+++ Language/Typo/ASTs.hs
@@ -1,4 +1,27 @@
 module Language.Typo.ASTs
-  (
+  ( Value(..)
+  , Surface(..)
+  , Op(..)
   ) where
 
+import Data.Word
+
+
+data Value
+  = Number Word
+  | Boolean Bool
+  | Id String
+  deriving ( Eq, Ord, Show )
+
+data Op
+  = Add | Sub | Mul | Div | Rem
+  | And | Or | Xor | Eq | Lt
+  deriving ( Eq, Ord, Enum, Bounded, Show )
+
+data Surface
+  = Val Value
+  | Let String Surface Surface
+  | App String [Surface]
+  | Bop Op Surface Surface
+  | Cond Surface Surface Surface
+  deriving ( Eq, Ord, Show )
